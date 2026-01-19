e51db351a8bec85614efa2913d132c5f08d9e49d
Immu <shaikimam37302@gmail.com>
2026-01-19 18:26:13 +0530

    Two_pointer_Approach

---
+++ Q05/main.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+
+    int i=0;
+    for(int j=0;j<n;j++){
+        if(arr[j]!=0){
+            int temp=arr[j];
+            arr[j]=arr[i];
+            arr[i]=temp;
+            i++;
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
