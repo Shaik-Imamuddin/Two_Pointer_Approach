af925c898c643301ef031936e2b6488ea3da291b
Immu <shaikimam37302@gmail.com>
2026-01-29 18:12:02 +0530

    Two_pointer_Approach

---
+++ Q07/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[] = new int[n];
+        
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        int start=0;
+
+        for(int i=0;i<n;i++){
+            if(arr[i]%2==0){
+                int temp=arr[i];
+
+                for(int j=i;j>start;j--){
+                    arr[j]=arr[j-1];
+                }
+                arr[start]=temp;
+                start++;
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+}
