3f12a99d1cecd6a896fd8f7c61a85de8f165d6a4
Immu <shaikimam37302@gmail.com>
2026-02-03 21:33:03 +0530

    Two_pointer_Approach

---
+++ Q08/main.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int i=0;    //count of unique elements 
+    for(int j=1;j<n;j++){
+        if(arr[j]!=arr[i]){
+            i++;
+            arr[i]=arr[j];
+        }
+    }
+    for(int k=0;k<=i;k++){
+        printf("%d ",arr[k]);
+    }
+    return 0;
+}
